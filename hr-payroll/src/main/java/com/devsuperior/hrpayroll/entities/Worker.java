8984ebf807fb7793c5b24d81ddf26704880e1a1e
henrique <henrique.augusto.rodri@gmail.com>
2021-09-16 21:41:11 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,32 @@
+package com.devsuperior.hrpayroll.entities;
+
+public class Worker {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+}
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,31 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long id, int days) {
-        return new Payment("Henrique", 200.0, days);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    public Payment getPayment(Long id, Integer days) {
+        Map<String, String> params = new HashMap<>();
+        params.put("id", id.toString());
+
+        Worker worker = restTemplate.getForObject(
+                workerHost + "/workers/{id}", Worker.class, params);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
