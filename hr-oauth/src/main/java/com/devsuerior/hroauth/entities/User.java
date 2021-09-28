1d50f194e86304829c956ceee41fcb18af356d59
henrique <henrique.augusto.rodri@gmail.com>
2021-09-28 12:46:33 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/devsuerior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devsuerior/hroauth/entities/User.java
@@ -1,9 +1,15 @@
 package com.devsuerior.hroauth.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User {
+public class User implements UserDetails {
 
     private Long id;
     private String name;
@@ -56,5 +62,37 @@ public class User {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
